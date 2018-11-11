43380e0407b97e01a5e1ea6c1344087f2c2254a3
audin30 <jjanolo@gmail.com>
2018-11-10 23:47:16 -0800

    11/10 11:47 PM

--- coincap_p1.py
+++ coincap_p1.py
@@ -3,3 +3,13 @@ import json
 import requests
 from datetime import datetime
 from prettytable import prettytable
+from colorama import Fore, Back, Style
+
+conver = 'USD'
+
+listings_url = 'https://api.coinmarkecap.com/v2/listings/?convert=' + convert
+
+request = requests.get(listings_url)
+results = reques.json()
+
+print(json.dumps(results, sort_keys=True, indent=4))
