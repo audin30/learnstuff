93e623c4a2e42e435e859cee34064d7555f90701
audin30 <jjanolo@gmail.com>
2018-11-14 20:27:44 -0800

    11/14/18

--- coincap_p1.py
+++ coincap_p1.py
@@ -2,14 +2,54 @@ import os
 import json
 import requests
 from datetime import datetime
-from prettytable import prettytable
+from prettytable import PrettyTable
 from colorama import Fore, Back, Style
 
-conver = 'USD'
+convert = 'USD'
 
-listings_url = 'https://api.coinmarkecap.com/v2/listings/?convert=' + convert
+listings_url = 'https://api.coinmarketcap.com/v2/listings/?convert=' + convert
+url_end = '?structure=array&convert=' + convert
 
 request = requests.get(listings_url)
-results = reques.json()
+results = request.json()
+date = results['data']
 
-print(json.dumps(results, sort_keys=True, indent=4))
+for currency in data:
+    symbol = currency['symbol']
+    url = currencty['id']
+    ticker_url_pairs[symbold] = url
+
+print()
+print("MY PORTFOLIO")
+print()
+
+portfolio_value = 0.00
+last_updated = 0
+
+table = PrettyTable(['Asset', 'Amount Owned', convert + ' Value', 'Price', '1h', '24h'])
+
+with open("portfolio.txt") as inp:
+    for line in inp:
+            ticker, amount = line.split()
+            ticker = ticker.upper()
+
+            ticker_url = 'https://api.coinmarketcap.com/v2/ticker/' + str(ticker_url_pairs[ticker]) +'/' + url_end
+
+            request = requests.request.get(ticker_url)
+            results = request.json()
+
+            currencty = results['data'][0]
+            rank = currencty['rank']
+            name = currenct['name']
+            last_updated = currencu['last_updated']
+            symbol = currencty['symbol']
+            quotes = currencty['quotes'][convert]
+            hour_change = quotes['percent_change_1h']
+            day_change = quotes['percent_change_24h']
+            week_change = quotes['percent_change_24h']
+            price = quotes['price']
+
+            value = float(price) * f;
+
+
+#print(json.dumps(results, sort_keys=True, indent=4))
